a1bfcca5cbf2c6689ffac7ca89bd1c43c32354bb
defetron27 <defetron@gmail.com>
2021-09-24 20:45:25 +0530

    initial commit

---
+++ app/src/main/java/com/deffe/max/vision/barcodescanning/BarcodeScanningProcessor.java
@@ -0,0 +1,65 @@
+package com.deffe.max.vision.barcodescanning;
+
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.deffe.max.vision.FrameMetadata;
+import com.deffe.max.vision.GraphicOverlay;
+import com.deffe.max.vision.VisionProcessorBase;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcode;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetector;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+
+import java.io.IOException;
+import java.util.List;
+
+/** Barcode Detector Demo. */
+public class BarcodeScanningProcessor extends VisionProcessorBase<List<FirebaseVisionBarcode>> {
+
+  private static final String TAG = "BarcodeScanningProcess";
+
+  private final FirebaseVisionBarcodeDetector detector;
+
+  public BarcodeScanningProcessor() {
+    // Note that if you know which format of barcode your app is dealing with, detection will be
+    // faster to specify the supported barcode formats one by one, e.g.
+    // new FirebaseVisionBarcodeDetectorOptions.Builder()
+    //     .setBarcodeFormats(irebaseVisionBarcode.FORMAT_QR_CODE)
+    //     .build();
+    detector = FirebaseVision.getInstance().getVisionBarcodeDetector();
+  }
+
+  @Override
+  public void stop() {
+    try {
+      detector.close();
+    } catch (IOException e) {
+      Log.e(TAG, "Exception thrown while trying to close Barcode Detector: " + e);
+    }
+  }
+
+  @Override
+  protected Task<List<FirebaseVisionBarcode>> detectInImage(FirebaseVisionImage image) {
+    return detector.detectInImage(image);
+  }
+
+  @Override
+  protected void onSuccess(
+      @NonNull List<FirebaseVisionBarcode> barcodes,
+      @NonNull FrameMetadata frameMetadata,
+      @NonNull GraphicOverlay graphicOverlay) {
+    graphicOverlay.clear();
+    for (int i = 0; i < barcodes.size(); ++i) {
+      FirebaseVisionBarcode barcode = barcodes.get(i);
+      BarcodeGraphic barcodeGraphic = new BarcodeGraphic(graphicOverlay, barcode);
+      graphicOverlay.add(barcodeGraphic);
+    }
+  }
+
+  @Override
+  protected void onFailure(@NonNull Exception e) {
+    Log.e(TAG, "BarcodeDetectionfailed " + e);
+  }
+}
---
+++ app/src/main/java/com/deffe/max/vision/cloudtextrecognition/CloudDocumentTextRecognitionProcessor.java
@@ -0,0 +1,47 @@
+package com.deffe.max.vision.cloudtextrecognition;
+
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.deffe.max.vision.FrameMetadata;
+import com.deffe.max.vision.GraphicOverlay;
+import com.deffe.max.vision.VisionProcessorBase;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.cloud.text.FirebaseVisionCloudDocumentTextDetector;
+import com.google.firebase.ml.vision.cloud.text.FirebaseVisionCloudText;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+
+/** Processor for the cloud document text detector demo. */
+public class CloudDocumentTextRecognitionProcessor extends VisionProcessorBase<FirebaseVisionCloudText> {
+
+  private static final String TAG = "CloudDocuTextRecProcess";
+
+  private final FirebaseVisionCloudDocumentTextDetector detector;
+
+  public CloudDocumentTextRecognitionProcessor() {
+    super();
+    detector = FirebaseVision.getInstance().getVisionCloudDocumentTextDetector();
+  }
+
+  @Override
+  protected Task<FirebaseVisionCloudText> detectInImage(FirebaseVisionImage image) {
+    return detector.detectInImage(image);
+  }
+
+  @Override
+  protected void onSuccess(
+      @NonNull FirebaseVisionCloudText text,
+      @NonNull FrameMetadata frameMetadata,
+      @NonNull GraphicOverlay graphicOverlay) {
+    graphicOverlay.clear();
+      Log.d(TAG, "detected text is: " + text.getText());
+      CloudTextGraphic textGraphic = new CloudTextGraphic(graphicOverlay, text);
+      graphicOverlay.add(textGraphic);
+  }
+
+  @Override
+  protected void onFailure(@NonNull Exception e) {
+    Log.w(TAG, "Cloud Document Text detection failed." + e);
+  }
+}
